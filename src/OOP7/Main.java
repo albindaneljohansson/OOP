0775aa2660be5118c609bedc07df8e7c6c8972eb
Albin Johansson <albin.daniel.johansson@gmail.com>
2022-10-20 13:52:40 +0200

    - Superclass setName test

--- src/OOP7/Main.java
+++ src/OOP7/Main.java
@@ -6,10 +6,10 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        Cirkel c1 = new Cirkel(2.3);
-        Cirkel c2 = new Cirkel(4.5);
-        LiksidigTriangel t1 = new LiksidigTriangel(3);
-        LiksidigTriangel t2 = new LiksidigTriangel(4.5);
+        Cirkel c1 = new Cirkel(2, 3, 2.3);
+        Cirkel c2 = new Cirkel(5, 6,4.5);
+        LiksidigTriangel t1 = new LiksidigTriangel(2, 3,4,3);
+        LiksidigTriangel t2 = new LiksidigTriangel(5, 6,4.5, 6);
 
         List<Figur> figurlista = new LinkedList<>();
 
